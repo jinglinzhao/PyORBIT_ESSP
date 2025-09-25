b28febfa1a6b8501d740a7e722fe3f1bd3b5208b
jinglinzhao <jinglinzhao@gmail.com>
2025-09-25 15:02:40 +0200

    proposal version

--- HD189567/Proposal/HD189567_2p.sh
+++ HD189567/Proposal/HD189567_2p.sh
@@ -5,15 +5,15 @@
 ### -- set the job Name -- 
 #BSUB -J HD189567_2p
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 16
+#BSUB -n 48
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "rusage[mem=1GB]"
 ### -- specify that we want the job to get killed if it exceeds 5GB per core/slot -- 
-#BSUB -M 5GB
+#BSUB -M 2GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 24:00
+#BSUB -W 1:00
 ### -- send notification at start -- 
 #BSUB -B 
 ### -- send notification at completion -- 
@@ -27,5 +27,26 @@ source /zhome/9d/b/207249/anaconda3/etc/profile.d/conda.sh
 # Activate Conda environment
 conda activate pyorbit
 
-pyorbit_run emcee HD189567_2p.yaml > HD189567_2p.log
-pyorbit_results emcee HD189567_2p.yaml -all > HD189567_2p.log
+# Add font fix
+export MPLBACKEND=Agg
+# Suppress font warnings
+export PYTHONWARNINGS="ignore::UserWarning:matplotlib"
+
+NAME="HD189567_2p"
+
+# Create output directory FIRST
+mkdir -p ${NAME}
+
+echo "Starting PyORBIT emcee run..."
+pyorbit_run emcee ${NAME}.yaml
+
+# Check if the first command succeeded before running results
+if [ $? -eq 0 ]; then
+    echo "PyORBIT run completed successfully, generating results..."
+    pyorbit_results emcee ${NAME}.yaml -all > ./${NAME}/${NAME}.log
+    cp ${NAME}.yaml ./${NAME}/
+    echo "Job completed successfully"
+else
+    echo "ERROR: PyORBIT run failed"
+    exit 1
+fi
